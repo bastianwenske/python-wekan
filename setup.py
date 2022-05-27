bfe642631826c8c4d70a60c8b04401bc53bc24fe
Bastian Wenske <bastianwenske@outlook.de>
2022-05-27 18:00:48 +0200

    improved package documentation

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='python-wekan',
-    version='0.1.1',
+    version='0.1.2',
     packages=['tests', 'wekan'],
     url='https://github.com/bastianwenske/python-wekan',
     download_url='https://github.com/sarumont/py-trello',
@@ -12,8 +12,6 @@ setup(
     description='This is a python client for interacting with the WeKan® REST-API',
     keywords='python',
     install_requires=["requests", "python-dateutil"],
-    long_description_content_type="text/x-rst",
-    long_description="""This is a python client for interacting with the WeKan® REST-API.
-                        Each WeKan object is represented by a corresponding python object.
-                     """
+    long_description_content_type="text/markdown",
+    long_description=open('README.md').read()
 )
