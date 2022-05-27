8700e3be4db68f3c8e585883ded6304b3a682745
Bastian Wenske <bastianwenske@outlook.de>
2022-05-27 17:42:43 +0200

    added long_description to setup.py

--- setup.py
+++ setup.py
@@ -12,4 +12,8 @@ setup(
     description='This is a python client for interacting with the WeKan® REST-API',
     keywords='python',
     install_requires=["requests", "python-dateutil"],
+    long_description_content_type="text/x-rst",
+    long_description="""This is a python client for interacting with the WeKan® REST-API.
+                        Each WeKan object is represented by a corresponding python object.
+                     """
 )
