1555e7913f12ea06ff2c11c9791a9478e02c171f
Bastian Wenske <bastianwenske@outlook.de>
2023-07-15 22:52:55 +0200

    Update to reference latest release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='python-wekan',
-    version='0.1.5',
+    version='0.1.6',
     packages=['tests', 'wekan'],
     url='https://github.com/bastianwenske/python-wekan',
     download_url='https://github.com/bastianwenske/python-wekan',
