ba6132c1d31ab1bf2e4dc6d20df4a328748fd770
Bastian Wenske <bastianwenske@outlook.de>
2024-11-16 13:30:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='python-wekan',
-    version='0.2.0',
+    version='0.2.1',
     packages=['tests', 'wekan'],
     url='https://github.com/bastianwenske/python-wekan',
     download_url='https://github.com/bastianwenske/python-wekan',
